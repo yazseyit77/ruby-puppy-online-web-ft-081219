5837bbb9002250f40ff9ce8b122aa6dd9c67b260
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-22 14:47:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -23,6 +23,6 @@ class Dog
   end
 
   def save
-
+    @@all << self
   end
 end
